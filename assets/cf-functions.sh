de6f0081b8ac90dc3ad9377e7b748313f55099c0
olekxd <bolszowski@gmail.com>
2019-01-15 16:25:25 +0100

    Update cf-functions.sh

--- assets/cf-functions.sh
+++ assets/cf-functions.sh
@@ -575,11 +575,12 @@ function cf_bind_service() {
 function cf_bind_route_service() {
   local domain=${1:?domain null or not set}
   local service_instance=${2:?service_instance null or not set}
-  local hostname=${3:?hostname null or not set}
+  local hostname=${3:-}
   local path=${4:-}
   local configuration=${5:-}
 
-  local args=("$domain" "$service_instance" "$hostname")
+  local args=("$domain" "$service_instance")
+  [ -n "$hostname" ] && args+=(--hostname "$hostname")
   [ -n "$path" ] && args+=(--path "$path")
   [ -n "$configuration" ] && args+=(-c "$configuration")
 
